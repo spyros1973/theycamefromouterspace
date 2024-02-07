a35fc082859da9d90f9e766ad23fa9a553c3d9a9
Spyros Paraschis <spyros1973@gmail.com>
2024-02-08 00:23:27 +0200

    Added another sound. Tweaked evemy edge behaviour.

--- sound_manager.go
+++ sound_manager.go
@@ -174,3 +174,26 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "hit2"
+  type: "sound"
+  data: "sound: \"/assets/sounds/Hit 2.wav\"\n"
+  "looping: 0\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  "pan: 0.0\n"
+  "speed: 1.0\n"
+  "loopcount: 0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
